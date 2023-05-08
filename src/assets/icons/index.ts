b93ad6943c0812237a5bc35ff72a23aaeefc8011
Ouzx <oguzhankandakoglu@gmail.com>
2023-05-08 18:11:29 +0300

    icons added

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,5 +2,5 @@ import Graph from "./Graph";
 import People from "./People";
 import PurpleBunny from "./PurpleBunny";
 import Trade from "./Trade";
-
-export { Graph, People, PurpleBunny, Trade };
+import UsedByMillionsSeperator from "./UsedByMillionsSeperator";
+export { Graph, People, PurpleBunny, Trade, UsedByMillionsSeperator };
