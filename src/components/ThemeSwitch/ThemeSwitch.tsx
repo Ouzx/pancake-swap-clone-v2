c173b3b1934564dae4b9b92912ab75e0c8aee882
Ouzx <oguzhankandakoglu@gmail.com>
2023-05-10 17:42:03 +0300

    Theme switch added

---
+++ src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+// import { useThemeContext } from "../../../../Context/ThemeContext";
+import { FiSun, FiMoon } from "react-icons/fi";
+
+import "./ThemeSwitch.scss";
+const ThemeSwitch = () => {
+  // const { theme, toggleTheme } = useThemeContext();
+  const [theme, setTheme] = useState(false);
+  const onClick = () => {
+    // toggleTheme();
+  };
+
+  return (
+    <div onClick={onClick} className={"switch-container"}>
+      <div onClick={onClick} className={"switch-container__wrapper"}>
+        <input type="checkbox" checked={theme} readOnly onClick={onClick} />
+        <div
+          aria-checked={theme}
+          onClick={onClick}
+          className="switch-container__wrapper__holder"
+        >
+          {!theme ? (
+            <FiSun
+              onClick={onClick}
+              size={20}
+              className="switch-container__wrapper__holder__icon"
+            />
+          ) : (
+            <FiMoon
+              size={20}
+              onClick={onClick}
+              className="switch-container__wrapper__holder__icon"
+            />
+          )}
+        </div>
+        <FiSun
+          onClick={onClick}
+          size={20}
+          className="switch-container__wrapper__icon --left"
+        />
+        <FiMoon
+          onClick={onClick}
+          size={20}
+          className="switch-container__wrapper__icon --right"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ThemeSwitch;
