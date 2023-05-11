2e0a470e483610591d9198ae043a388bff9cc63b
Ouzx <oguzhankandakoglu@gmail.com>
2023-05-11 17:29:51 +0300

    social icons added

--- src/sections/oguzhan/sections/Footer/Footer.tsx
+++ src/sections/oguzhan/sections/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import Button from "./Components/Button/Button";
 import ThemeSwitch from "../../../../components/ThemeSwitch/ThemeSwitch";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import LanguageSelector from "../../../../components/LanguageSelector/LanguageSelector";
-
+import SocialIcons from "./Components/SocialIcons/SocialIcons";
 const Footer = () => {
   return (
     <div className="footer-container">
@@ -26,77 +26,85 @@ const Footer = () => {
         <LanguageSelector />
       </div>
 
-      <div className="seperator-component row links">
-        <div className="column">
-          <h3>ABOUT</h3>
-          <ul>
-            <li>
-              <a href="#" className="highlighted-link">
-                Contact
-              </a>
-            </li>
-            <li>
-              <a href="#">Brand</a>
-            </li>
-            <li>
-              <a href="#">Blog</a>
-            </li>
-            <li>
-              <a href="#">Community</a>
-            </li>
-            <li>
-              <a href="#">Litepaper</a>
-            </li>
-            <li>
-              <a href="#">Terms Of Service</a>
-            </li>
-          </ul>
+      <div className="seperator-component row ">
+        <div className="links">
+          <div className="column">
+            <h3>ABOUT</h3>
+            <ul>
+              <li>
+                <a href="#" className="highlighted-link">
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a href="#">Brand</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Community</a>
+              </li>
+              <li>
+                <a href="#">Litepaper</a>
+              </li>
+              <li>
+                <a href="#">Terms Of Service</a>
+              </li>
+            </ul>
+          </div>
+          <div className="column">
+            <h3>ABOUT</h3>
+            <ul>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">Brand</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Community</a>
+              </li>
+              <li>
+                <a href="#">Litepaper</a>
+              </li>
+              <li>
+                <a href="#">Terms Of Service</a>
+              </li>
+            </ul>
+          </div>
+          <div className="column">
+            <h3>ABOUT</h3>
+            <ul>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">Brand</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Community</a>
+              </li>
+              <li>
+                <a href="#">Litepaper</a>
+              </li>
+              <li>
+                <a href="#">Terms Of Service</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="column">
-          <h3>ABOUT</h3>
-          <ul>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              <a href="#">Brand</a>
-            </li>
-            <li>
-              <a href="#">Blog</a>
-            </li>
-            <li>
-              <a href="#">Community</a>
-            </li>
-            <li>
-              <a href="#">Litepaper</a>
-            </li>
-            <li>
-              <a href="#">Terms Of Service</a>
-            </li>
-          </ul>
-        </div>
-        <div className="column">
-          <h3>ABOUT</h3>
-          <ul>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              <a href="#">Brand</a>
-            </li>
-            <li>
-              <a href="#">Blog</a>
-            </li>
-            <li>
-              <a href="#">Community</a>
-            </li>
-            <li>
-              <a href="#">Litepaper</a>
-            </li>
-            <li>
-              <a href="#">Terms Of Service</a>
-            </li>
-          </ul>
+      </div>
+
+      <div className="row">
+        <div className="social-media">
+          <SocialIcons />
         </div>
       </div>
     </div>
