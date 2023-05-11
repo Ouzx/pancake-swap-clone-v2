d1f99b84dce081f73ca23fdd74d6ba8b2f7a8ecc
Ouzx <oguzhankandakoglu@gmail.com>
2023-05-11 18:59:28 +0300

    Oguz's part destructured

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-
-import Oguzhan from "./sections/oguzhan/Index";
 import Khaled from "./sections/khaled/Index";
 import Ulas from "./sections/ulas/Index";
 import Ozkan from "./sections/ozkan/Index";
@@ -10,11 +7,19 @@ import "./App.scss";
 import BackToTop from "./components/BackToTop/BackToTop";
 import DarkMode from "./components/DarkMode";
 import { ThemeProvider } from "./context/ThemeContext";
+import UsedByMillions from "./sections/oguzhan/sections/UsedByMillions/UsedByMillions";
+import Footer from "./sections/oguzhan/sections/Footer/Footer";
+
 function App() {
   return (
     <ThemeProvider>
       <div className="App">
-        <Oguzhan />
+        {/* OZKAN'S PART HERE */}
+        <UsedByMillions />
+        {/* OTHERS HERE */}
+        <Footer />
+
+        {/* UTILS */}
         <BackToTop />
         <DarkMode />
       </div>
