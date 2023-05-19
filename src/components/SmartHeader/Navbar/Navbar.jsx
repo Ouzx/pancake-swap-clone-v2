14d482fe2b5de31b997c021d2d833ac17e879fc4
Ouzx <oguzhankandakoglu@gmail.com>
2023-05-19 14:59:53 +0300

    some updates

--- src/components/SmartHeader/Navbar/Navbar.jsx
+++ src/components/SmartHeader/Navbar/Navbar.jsx
@@ -15,18 +15,21 @@ import HeaderLogoMini from "./HeaderLogoMini";
 import NavbarList from "./NavbarLists/NavbarList";
 import NavbarLanguageSelector from "./LanguageSelector/NavbarLanguageSelector";
 
+import { openSettings } from "../../Settings/Settings";
+
 const Navbar = () => {
   return (
-    <div className="navbar-container">
-      <div className="links">
-        <div className="navbar-logo">
-          <HeaderLogoMini width={32} />
-          <HeaderLogo width={160} />
-        </div>
+    <div className="navbar-wrapper">
+      <div className="navbar-container">
+        <div className="links">
+          <div className="navbar-logo">
+            <HeaderLogoMini width={32} />
+            <HeaderLogo width={160} />
+          </div>
 
-        <nav>
-          <span className="navSpan">
-                Trade
+          <nav>
+            <span className="navSpan">
+              Trade
               <div className="lists-inner">
                 <NavbarList
                   barList={[
@@ -37,48 +40,36 @@ const Navbar = () => {
                     "Limit(V2)",
                   ]}
                 />
-            </div>
-          </span>
-          <span className="navSpan">
+              </div>
+            </span>
+            <span className="navSpan">
               Earn
               <div className="lists-inner">
-                <NavbarList
-                  barList={[
-                    "Farms",
-                    "Pools",
-                    "Liquid Staking",
-                  ]}
-                />
-            </div>
-          </span>
-          <span className="navSpan">
-                Win
+                <NavbarList barList={["Farms", "Pools", "Liquid Staking"]} />
+              </div>
+            </span>
+            <span className="navSpan">
+              Win
               <div className="lists-inner">
                 <NavbarList
                   barList={[
                     "Trading Competition",
                     "Prediction (BETA)",
                     "Lottery",
-                    "Pottery (BETA)"
+                    "Pottery (BETA)",
                   ]}
                 />
-            </div>
-          </span>
-          <span className="navSpan">
-                NFT
+              </div>
+            </span>
+            <span className="navSpan">
+              NFT
               <div className="lists-inner">
-                <NavbarList
-                  barList={[
-                    "Overview",
-                    "Collections",
-                    "Activity"
-                  ]}
-                />
-            </div>
-          </span>
-          <span className="navSpan">
-            <FiMoreHorizontal size={20} />
-            <div className="lists-inner">
+                <NavbarList barList={["Overview", "Collections", "Activity"]} />
+              </div>
+            </span>
+            <span className="navSpan">
+              <FiMoreHorizontal size={20} />
+              <div className="lists-inner">
                 <NavbarList
                   barList={[
                     "Info",
@@ -87,66 +78,71 @@ const Navbar = () => {
                     "Voting",
                     "Leaderboard",
                     "Blog",
-                    "Docs"
+                    "Docs",
                   ]}
                 />
-            </div>
-          </span>
-        </nav>
-      </div>
-      <div className="buttons">
-        <div className="coin">
-          <PsCoin />
+              </div>
+            </span>
+          </nav>
         </div>
-        <span></span>
-        {/* <TbWorld style={{ cursor: "pointer" }} size={24} /> */}
-        <NavbarLanguageSelector />
-        <GoGear style={{ cursor: "pointer" }} size={24} />
+        <div className="buttons">
+          <div className="coin">
+            <PsCoin />
+          </div>
+          <span></span>
+          {/* <TbWorld style={{ cursor: "pointer" }} size={24} /> */}
+          <NavbarLanguageSelector />
+          <GoGear
+            onClick={openSettings}
+            style={{ cursor: "pointer" }}
+            size={24}
+          />
 
-        <div className="binance-button small">
-          <Button text="" style={{ padding: "0.3rem 1rem" }}>
-            <AiFillCaretDown size={18} />
-          </Button>
-          <img src={binanceImg} alt="binance" />
-        </div>
+          <div className="binance-button small">
+            <Button text="" style={{ padding: "0.3rem 1rem" }}>
+              <AiFillCaretDown size={18} />
+            </Button>
+            <img src={binanceImg} alt="binance" />
+          </div>
 
-        <div className="binance-button medium">
-          <Button
-            text="BNB"
-            style={{
-              padding: "0.3rem 1rem",
-              paddingLeft: "2.4rem",
-              paddingRight: "1rem",
-            }}
-          >
-            <AiFillCaretDown size={18} />
-          </Button>
-          <img src={binanceImg} alt="binance" />
-        </div>
-        <div className="binance-button big">
-          <Button
-            text="BNB Smart Chain"
-            style={{
-              padding: "0.3rem 1rem",
-              paddingLeft: "2.4rem",
-              paddingRight: "1rem",
-            }}
-          >
-            <AiFillCaretDown size={18} />
-          </Button>
-          <img src={binanceImg} alt="binance" />
-        </div>
-        <div className="small">
-          <Button text="Connect" style={{ padding: "0.3rem 1rem" }}></Button>
-        </div>
-        <div className="medium">
-          <Button text="Connect" style={{ padding: "0.3rem 1rem" }}></Button>
-        </div>
-        <div className="big">
-          <Button
-            text="Connect Wallet"
-            style={{ padding: "0.3rem 1rem" }}
-          ></Button>
+          <div className="binance-button medium">
+            <Button
+              text="BNB"
+              style={{
+                padding: "0.3rem 1rem",
+                paddingLeft: "2.4rem",
+                paddingRight: "1rem",
+              }}
+            >
+              <AiFillCaretDown size={18} />
+            </Button>
+            <img src={binanceImg} alt="binance" />
+          </div>
+          <div className="binance-button big">
+            <Button
+              text="BNB Smart Chain"
+              style={{
+                padding: "0.3rem 1rem",
+                paddingLeft: "2.4rem",
+                paddingRight: "1rem",
+              }}
+            >
+              <AiFillCaretDown size={18} />
+            </Button>
+            <img src={binanceImg} alt="binance" />
+          </div>
+          <div className="small">
+            <Button text="Connect" style={{ padding: "0.3rem 1rem" }}></Button>
+          </div>
+          <div className="medium">
+            <Button text="Connect" style={{ padding: "0.3rem 1rem" }}></Button>
+          </div>
+          <div className="big">
+            <Button
+              text="Connect Wallet"
+              style={{ padding: "0.3rem 1rem" }}
+            ></Button>
+          </div>
         </div>
       </div>
     </div>
