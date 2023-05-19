c548a987549b660040b65f1149c402e7fcb3231e
Khaled Sherif <khsherif13@gmail.com>
2023-05-19 14:10:07 +0300

    Fixed Dark Mode issues for Cards
    Co-authored-by: Oğuzhan Kandakoğlu <Ouzx@users.noreply.github.com>

--- src/sections/khaled/Index.jsx
+++ src/sections/khaled/Index.jsx
@@ -1,8 +1,10 @@
+import ThemeSwitch from "../../components/ThemeSwitch/ThemeSwitch";
 import Container from "./components/container/container.component";
 import SyrupPoolsAndFarmsParentContainer from "./components/syrup-pools-and-farms-parent-container/syrup-pools-and-farms-parent-container.component";
 
 const Index = () => {
   return (<div className='home-3'>
+    {/* <ThemeSwitch /> */}
     <Container />
   </div>);
 };
