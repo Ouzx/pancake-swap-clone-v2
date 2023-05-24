a14b3ae739dad6916589134952b6c726e6a39133
Ouzx <oguzhankandakoglu@gmail.com>
2023-05-24 17:08:14 +0300

    Ulas's sections removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Khaled from "./sections/khaled/Index";
-import Ulas from "./sections/ulas/Index";
+
 import Ozkan from "./sections/ozkan/Index";
 import Ahmet from "./sections/ahmet/Index";
 
@@ -11,9 +11,6 @@ import UsedByMillions from "./sections/oguzhan/sections/UsedByMillions/UsedByMil
 import Footer from "./sections/oguzhan/sections/Footer/Footer";
 import Settings, { openSettings } from "./components/Settings/Settings";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-import TradeAnything from "./sections/ulas/sections/Trade/TradeAnything";
-import Earn from "./sections/ulas/sections/Earn/Earn";
 
 const queryClient = new QueryClient();
 
@@ -29,8 +26,8 @@ function App() {
           <UsedByMillions />
 
           {/* OTHERS HERE */}
-          <TradeAnything />
-          <Earn />
+
+          {/* ULAS'S PART*/}
           <Khaled />
           <Ahmet />
 
